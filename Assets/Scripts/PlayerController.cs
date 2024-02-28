7f84e02b820b0a24763e4e499da9950228927ddf
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-02-28 11:56:06 +0000

    Added placeholder visuals, began animating

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,13 +12,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float movementSpeed = 10f;
 
     Rigidbody2D playerRigidbody;
+    Animator playerAnimator;
     PlayerInput playerInput;
     InputAction moveAction;
     InputAction interactAction;
+    
 
     void Awake() 
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
         interactAction = playerInput.actions["Interact"];
@@ -35,7 +38,29 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = moveAction.ReadValue<Vector2>();
         Vector2 currentMovementVector = new Vector2(inputVector.x * movementSpeed, inputVector.y * movementSpeed);
 
+        SetMovementAnimation(currentMovementVector);
         playerRigidbody.velocity = currentMovementVector;
+    }
+
+    void SetMovementAnimation(Vector2 currentMovementVector)
+    {
+        if (currentMovementVector.x == 0 && currentMovementVector.y == 0)
+        {
+            playerAnimator.SetBool("SRun", false);
+            playerAnimator.SetBool("WRun", false);
+        }
+
+        if (currentMovementVector.y < 0)
+        {
+            playerAnimator.SetBool("SRun", true);
+            playerAnimator.SetBool("WRun", false);
+        }
+
+        if (currentMovementVector.y > 0)
+        {
+            playerAnimator.SetBool("WRun", true);
+            playerAnimator.SetBool("SRun", false);
+        }
     }
 
     void Interact()
