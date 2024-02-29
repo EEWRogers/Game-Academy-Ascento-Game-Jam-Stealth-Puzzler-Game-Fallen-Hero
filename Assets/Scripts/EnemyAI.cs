1c45a9db0d80cec4a89a65fdd4ffb6ee1b10242a
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-02-29 17:03:23 +0000

    Implemented a patrol system allowing for waypoints to be placed in the world and for enemies to follow them.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,20 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] LayerMask whatToLookFor;
+    [SerializeField] PatrolPath patrolPath;
+    [SerializeField] float waypointTolerance = 1f;
+    [SerializeField] float guardSpeed = 0.5f;
     PolygonCollider2D enemyVisionCollider;
     SpriteRenderer enemySpriteRenderer;
+    Transform enemyCentreTransform;
     
-
     bool playerSeen = false;
+    int currentWaypointIndex = 0;
 
     void Awake() 
     {
         enemyVisionCollider = GetComponentInChildren<PolygonCollider2D>();
         enemySpriteRenderer = GetComponent<SpriteRenderer>();
+        enemyCentreTransform = transform.GetChild(0).transform;
     }
 
     void Update() 
@@ -23,6 +30,42 @@ public class EnemyAI : MonoBehaviour
         {
             enemySpriteRenderer.color = Color.green;
         }
+
+        if (patrolPath != null)
+        {
+            if (AtWaypoint())
+            {
+                GetNextWaypoint();
+            }
+            MoveToLocation(GetCurrentWaypoint());
+        }
+
+    }
+
+    Vector2 GetCurrentWaypoint()
+    {
+        return patrolPath.GetWaypointPosition(currentWaypointIndex);
+    }
+
+    void GetNextWaypoint()
+    {
+        currentWaypointIndex = patrolPath.GetNextWaypoint(currentWaypointIndex);
+    }
+
+    bool AtWaypoint()
+    {
+        float distanceToWaypoint = Vector2.Distance(transform.position, GetCurrentWaypoint());
+        return distanceToWaypoint < waypointTolerance;
+    }
+
+    void MoveToLocation(Vector2 destination)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, guardSpeed * Time.deltaTime);
+    }
+
+    void RotateViewTowardsDestination()
+    {
+        
     }
 
     void OnTriggerEnter2D(Collider2D other) 
---
+++ Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PatrolPath : MonoBehaviour
+{
+    [SerializeField] float waypointSize = 0.1f;
+
+    void OnDrawGizmos() 
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            int nextWaypoint = GetNextWaypoint(i);
+
+            Gizmos.DrawSphere(GetWaypointPosition(i), waypointSize);
+            Gizmos.DrawLine(GetWaypointPosition(i), GetWaypointPosition(nextWaypoint));
+        }
+    }
+
+    public int GetNextWaypoint(int i)
+    {
+        if (i == transform.childCount - 1)
+        {
+            return 0;
+        }
+
+        return i + 1;
+    }
+
+    public Vector2 GetWaypointPosition(int i)
+    {
+        return transform.GetChild(i).position;
+    }
+}
