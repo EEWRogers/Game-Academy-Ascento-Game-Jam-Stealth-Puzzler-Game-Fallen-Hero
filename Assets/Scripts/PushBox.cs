c4799cd3b8a7466178db2e3b4308de1c33550b6e
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-03-01 15:35:40 +0000

    Began implementing more specific box pushing

--- Assets/Scripts/PushBox.cs
+++ Assets/Scripts/PushBox.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 public class PushBox : MonoBehaviour
 {
-    public ColliderSide sideBeingPushed = ColliderSide.NONE;
-
-    void Awake() 
-    {
-        
-    }
+    [SerializeField] float timeBeforePushing = 1f;
+    [SerializeField] float boxSpeed = 10f;
+    float pushTimer;
+    bool beingPushed;
+    ColliderSide sideBeingPushed = ColliderSide.NONE;
 
     void Update() 
     {
         CheckWhichSideBeingPushed();
+        MoveBox();
     }
 
     void CheckWhichSideBeingPushed()
@@ -28,6 +28,55 @@ public class PushBox : MonoBehaviour
             if (currentSide.IsBeingPushed)
             {
                 sideBeingPushed = currentSide.ColliderSide;
+            }
+        }
+    }
+
+    void MoveBox()
+    {
+        if (sideBeingPushed != ColliderSide.NONE)
+        {
+            Vector3 originalPosition = transform.position;
+
+            if (!beingPushed)
+            {
+                pushTimer = timeBeforePushing;
+                beingPushed = true;
+            }
+
+            pushTimer -= Time.deltaTime;
+            Debug.Log(pushTimer);
+
+            if (pushTimer <= Mathf.Epsilon)
+            {
+                Vector3 direction = Vector3.zero;
+
+                switch (sideBeingPushed)
+                {
+                    case ColliderSide.TOP:
+                    direction = Vector3.down;
+                    break;
+
+                    case ColliderSide.LEFT:
+                    direction = Vector3.right;
+                    break;
+
+                    case ColliderSide.RIGHT:
+                    direction = Vector3.left;
+                    break;
+
+                    case ColliderSide.BOTTOM:
+                    direction = Vector3.up;
+                    break;
+                }
+
+                transform.Translate(direction * Time.deltaTime * boxSpeed);
+
+                if (Vector3.Distance(transform.position, originalPosition + direction) >= 1.0f)
+                {
+                    transform.position = originalPosition + direction;
+                    beingPushed = false;
+                }
             }
         }
     }
