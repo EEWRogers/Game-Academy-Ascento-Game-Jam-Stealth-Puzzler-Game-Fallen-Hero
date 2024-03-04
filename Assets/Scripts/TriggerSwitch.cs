dfa7715d143095a481ffc856e027e71a1cfc3cdd
Elliott Rogers <elliott.e.w.rogers@gmail.com>
2024-03-04 11:42:43 +0000

    Created a simple switch which activates when a box is placed on it

---
+++ Assets/Scripts/TriggerSwitch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TriggerSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject objectConnectedToSwitch;
+    public bool isTriggered = false;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PushableObject"))
+        {
+            isTriggered = true;
+            Debug.Log("Box has triggered me!");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("PushableObject"))
+        {
+            isTriggered = false;
+            Debug.Log("Box has stopped triggering me!");
+        }
+    }
+}
